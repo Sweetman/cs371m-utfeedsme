ba251265f84a526a3959060dc95a8b671a862d64
James <jamestsweetman@gmail.com>
2014-12-01 00:15:37 -0600

    hopefully this works

--- UTFeedsMe/src/com/example/utfeedsme/AddEvent.java
+++ UTFeedsMe/src/com/example/utfeedsme/AddEvent.java
@@ -1,7 +1,6 @@
 package com.example.utfeedsme;
 
 import java.util.Calendar;
-
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
@@ -12,19 +11,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+//import android.widget.TextView;
 import android.widget.TimePicker;
-
 import com.parse.ParseObject;
 
 public class AddEvent extends FragmentActivity {
-	
-//	private final int MINS_PER_DAY = 1440;
-//	protected RecordsDataSource dataSource;
+
+	// private final int MINS_PER_DAY = 1440;
+	// protected RecordsDataSource dataSource;
 	Button add_event;
 	EditText event_name, event_where, event_food;
 	Button event_time;
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.v("addevent", "entered oncreate for addevent");
@@ -35,26 +33,20 @@ public class AddEvent extends FragmentActivity {
 		event_time = (Button) findViewById(R.id.add_event_time);
 		event_food = (EditText) findViewById(R.id.add_event_food);
 		Button add_event = (Button) findViewById(R.id.add_event_btn);
-		
 		event_time.setOnClickListener(new View.OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
-        	    DialogFragment newFragment = new TimePickerFragment();
-        	    newFragment.show(getSupportFragmentManager(), "timePicker");
-
-				
+				DialogFragment newFragment = new TimePickerFragment();
+				newFragment.show(getSupportFragmentManager(), "timePicker");
 			}
 		});
-		
 		add_event.setOnClickListener(new View.OnClickListener() {
-			
 			@Override
 			public void onClick(View v) {
 				Log.v("addevent", "event: " + event_name.getText().toString()+
-						", " + event_where.getText().toString() + 
-						", " + event_time.getText().toString() +
-						", " + event_food.getText().toString());
+				", " + event_where.getText().toString() +
+				", " + event_time.getText().toString() +
+				", " + event_food.getText().toString());
 				ParseObject gameScore = new ParseObject("FoodEvent");
 				gameScore.put("event", event_name.getText().toString());
 				gameScore.put("time", event_time.getText().toString());
@@ -64,8 +56,9 @@ public class AddEvent extends FragmentActivity {
 			}
 		});
 	}
-    public class TimePickerFragment extends DialogFragment
-    implements TimePickerDialog.OnTimeSetListener {
+	
+	public class TimePickerFragment extends DialogFragment
+	implements TimePickerDialog.OnTimeSetListener {
 
 		@Override
 		public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -74,18 +67,15 @@ public class AddEvent extends FragmentActivity {
 			final Calendar c = Calendar.getInstance();
 			int hour = c.get(Calendar.HOUR_OF_DAY);
 			int minute = c.get(Calendar.MINUTE);
-		
 			// Create a new instance of TimePickerDialog and return it
 			return new TimePickerDialog(getActivity(), this, hour, minute,
-			DateFormat.is24HourFormat(getActivity()));
+					DateFormat.is24HourFormat(getActivity()));
 		}
-		
 		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
 			//change text to hour..
 			//see what calls this
 			// Do something with the time chosen by the user
-	        Button button = (Button) findViewById(R.id.add_event_time);
-
+			Button button = (Button) findViewById(R.id.add_event_time);
 			button.setText(new StringBuilder().append(String.valueOf(hourOfDay))
 					.append(":").append(String.valueOf(minute)));
 		}
--- UTFeedsMe/src/com/example/utfeedsme/StartScreen.java
+++ UTFeedsMe/src/com/example/utfeedsme/StartScreen.java
@@ -1,31 +1,23 @@
 package com.example.utfeedsme;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.os.Build;
-
-import java.util.List;
-import java.util.Random;
+import android.widget.ImageButton;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
 
 public class StartScreen extends Activity {
 	
-	protected RecordsDataSource dataSource;
+	private final static String TAG = "StartScreen";
+	
+	//protected RecordsDataSource dataSource;
 		
-	Button happening_now_btn, near_you_btn, all_events_btn, add_event_btn;
+	ImageButton happening_now_btn, near_you_btn, all_events_btn, add_event_btn;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +29,13 @@ public class StartScreen extends Activity {
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
         testObject.saveInBackground();
-        dataSource = new RecordsDataSource(this);
-        dataSource.open();
+        //dataSource = new RecordsDataSource(this);
+        //dataSource.open();
         
-        happening_now_btn = (Button) findViewById(R.id.happening_now);
-        near_you_btn = (Button) findViewById(R.id.near_you);
-        all_events_btn = (Button) findViewById(R.id.all_events);
-        add_event_btn = (Button) findViewById(R.id.add_event);
+        happening_now_btn = (ImageButton) findViewById(R.id.happening_now);
+        near_you_btn = (ImageButton) findViewById(R.id.near_you);
+        all_events_btn = (ImageButton) findViewById(R.id.all_events);
+        add_event_btn = (ImageButton) findViewById(R.id.add_event);
         
         happening_now_btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -61,16 +53,16 @@ public class StartScreen extends Activity {
         
         all_events_btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Intent openNearYou = new Intent("com.example.utfeedsme.ALLEVENTS");
-				startActivity(openNearYou);
+				Intent openAllEvents = new Intent("com.example.utfeedsme.ALLEVENTS");
+				startActivity(openAllEvents);
 			}
 		});
         
         add_event_btn.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				Intent openNearYou = new Intent("com.example.utfeedsme.ADDEVENT");
-				Log.v("addevent", "yoooo we pressed the add event button");
-				startActivity(openNearYou);
+				Intent openAddEvent = new Intent("com.example.utfeedsme.ADDEVENT");
+				Log.v(TAG, "yoooo we pressed the add event button");
+				startActivity(openAddEvent);
 			}
 		});
 
@@ -82,7 +74,7 @@ public class StartScreen extends Activity {
         getMenuInflater().inflate(R.menu.start_screen, menu);
         return true;
     }
-
+/*
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -90,21 +82,26 @@ public class StartScreen extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+        	Log.d(TAG, "selected settings");
             return true;
+        }
+        if (id == R.id.sort_pref) {
+        	Log.d(TAG, "selected to sort");
+        	return true;
         }
         return super.onOptionsItemSelected(item);
     }
-    
+  */  
 
     @Override
     protected void onResume() {
-      dataSource.open();
+      //dataSource.open();
       super.onResume();
     }
 
     @Override
     protected void onPause() {
-      dataSource.close();
+      //dataSource.close();
       super.onPause();
     }
 }

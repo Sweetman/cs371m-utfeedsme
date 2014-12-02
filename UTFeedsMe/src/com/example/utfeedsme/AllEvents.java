9a4e001b9b8ee2ab8e7fd336a3a867f0d5955f84
Daniel <DanielJHaas@gmail.com>
2014-12-01 22:44:13 -0600

    changed add events screen format

--- UTFeedsMe/src/com/example/utfeedsme/AllEvents.java
+++ UTFeedsMe/src/com/example/utfeedsme/AllEvents.java
@@ -1,34 +1,60 @@
 package com.example.utfeedsme;
 
-/*
- * Database code derived from Lars Vogel
- * http://www.vogella.com/tutorials/AndroidSQLite/article.html
- */
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
 
-import java.util.List;
-import java.util.Random;
-
+import com.parse.Parse;
 import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
 
-import android.app.ListActivity;
-import android.view.View;
-import android.widget.ArrayAdapter;
+public class AllEvents extends Activity {
 
-public class AllEvents extends ListActivity {
-	
-	protected RecordsDataSource dataSource;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.all_events);
-		ParseQueryAdapter<ParseObject> mainAdapter = new ParseQueryAdapter<ParseObject>(this, "FoodEvent");
-		mainAdapter.setTextKey("event");
-		System.out.println("hello");
-		setListAdapter(mainAdapter);
-	}
+    private ParseQueryAdapter<ParseObject> mainAdapter;
+    private CustomAdapter urgentTodosAdapter;
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_events);
+
+        // Initialize main ParseQueryAdapter
+        mainAdapter = new ParseQueryAdapter<ParseObject>(this, "FoodEvent");
+        mainAdapter.setTextKey("event");
+        mainAdapter.setImageKey("Image");
+
+        // Initialize the subclass of ParseQueryAdapter
+        urgentTodosAdapter = new CustomAdapter(this);
+
+        // Initialize ListView and set initial view to mainAdapter
+        listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(mainAdapter);
+        mainAdapter.loadObjects();
+
+        listView.setAdapter(urgentTodosAdapter);
+        urgentTodosAdapter.loadObjects();
+        
+        
+        // Initialize toggle button
+        //Button toggleButton = (Button) findViewById(R.id.toggleButton);
+        //toggleButton.setOnClickListener(new OnClickListener() {
+        /*
+            @Override
+            public void onClick(View v) {
+                if (listView.getAdapter() == mainAdapter) {
+                    listView.setAdapter(urgentTodosAdapter);
+                    urgentTodosAdapter.loadObjects();
+                } else {
+                    listView.setAdapter(mainAdapter);
+                    mainAdapter.loadObjects();
+                }
+            }
+
+        });*/
+    }
+
 }
